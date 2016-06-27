fc6cfbb2f47807d72e80b182ccec2e5161bb028a
Christoph <chris@inferno.nadir.org>
2016-06-27 15:42:36 +0200

    use bull to queue mails

--- service/util/mails.js
+++ service/util/mails.js
@@ -5,11 +5,13 @@ const nodemailer = require('nodemailer');
 const sendmailTransport = require('nodemailer-sendmail-transport');
 const config = require('config');
 const pug = require('pug');
-
+const Queue = require('bull');
 const editUrlHelper = require('../../domain/editUrlHelper');
+
 
 let service = {};
 service._nodemailer = nodemailer;
+service._mailQueue = Queue('mail sending',6379,config.get('redishost'));
 
 service.sendStatusEmail = (participant, subject, pugfile) => {
   pug.renderFile(pugfile,
@@ -23,16 +25,21 @@ service.sendEmail = (address, subject, text, error) => {
   if (error) {
     console.error(error);
   } else {
+    service._mailQueue.add({address: address, subject: subject, text:text});
+  }
+};
+
+service._mailQueue.process(function(job,done){
     let transporter = service._nodemailer.createTransport(sendmailTransport({
       path: '/usr/sbin/sendmail'
     }));
     transporter.sendMail({
       from: config.get('contact.email'),
-      to: address,
-      subject: subject,
-      html: text
+      to: job.data.address,
+      subject: job.data.subject,
+      html: job.data.text
+    }).then(() => {
+    done();
     });
-  }
-};
-
+});
 module.exports = service;
