26b5961af69482118ceb1053cadf680422a5e84f
Christoph <chris@inferno.nadir.org>
2016-06-28 15:18:02 +0200

    send request on input

--- scanner/scan.js
+++ scanner/scan.js
@@ -1,11 +1,20 @@
 const readline = require('readline');
+const config = require('config');
+const request = require('request');
 
+var apiurl = config.get('pace-url')+'/api/scan';
 const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
 rl.on('line', (input) => {
-    console.log(`Received: ${input}`);
+    request.post(apiurl, {form:{startnumber:input,time:Date.now()}})
+    .on('response', function(response) {
+      console.log(response.statusCode) // 200 
+    })
+    .on('error', function(err) {
+       console.log(err)
+    })
 });
 
