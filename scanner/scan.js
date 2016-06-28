346554579ac617cee9b1ae76cf4b7c35422fa9c4
Christoph <chris@inferno.nadir.org>
2016-06-28 23:01:18 +0200

    manage input
    * listen on keyboards seperately
    * make keyboards configurable
    * only listen on keys we are interested in

--- scanner/scan.js
+++ scanner/scan.js
@@ -1,20 +1,29 @@
 const readline = require('readline');
 const config = require('config');
 const request = require('request');
+const LinuxInputListener = require('linux-input-device');
 
 var apiurl = config.get('pace-url')+'/api/scan';
-const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout
+var keylist = [2,3,4,5,6,7,8,9,10,11]
+var number = {};
+var events = config.get('events')
+var input = {};
+events.forEach(function(event) {
+ input[event] = new LinuxInputListener('/dev/input/event'+event);
+ input[event].on('state', function(value, key, kind) {
+	if (value == true) {
+		handle_keys(key,event)
+	}
+   });
 });
 
-rl.on('line', (input) => {
-    request.post(apiurl, {form:{startnumber:input,time:Date.now()}})
-    .on('response', function(response) {
-      console.log(response.statusCode) // 200 
-    })
-    .on('error', function(err) {
-       console.log(err)
-    })
-});
+function handle_keys(key,event) {
+ if (keylist.indexOf(key) >= 0)  {
+      number[event] = "" + number[event] + (key-1)
+    }
+    if (key == 28) {
+	console.log(number[event]);
+	number[event] = '';
+    }
+};
 
